c24585641e48d0024bf044e40139f13f6f53f4aa
kindyear <kindyear@qq.com>
2023-09-22 13:00:57 +0800

    Create Base file

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+    app.js
+    Simple-Channel-Backend
+    简易频道系统后端
+    Created time: 2023.9.22
+    Author: KINDYEAR
+    https://github.com/kindyear/Simple-Channel-Backend
+
+*/
+
+const {logTime} = require('./src/lib/logTime');
+const axios = require('axios')
+console.log(`${logTime()} Service starting...`);
---
+++ src/lib/logTime.js
@@ -0,0 +1,20 @@
+/*
+
+    logTime.js
+    日志时间输出功能
+
+*/
+
+function logTime() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `[${year}-${month}-${day} ${hours}:${minutes}:${seconds}]`;
+}
+
+module.exports = {logTime};
